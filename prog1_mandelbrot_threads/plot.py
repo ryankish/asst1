9e8830ed9a3b897dd3af702fa1a134a654a74b56
Ryan Kish <ryan.douglas.kish@gmail.com>
2024-12-12 23:42:15 -0600

    add prog1 view2

--- prog1_mandelbrot_threads/plot.py
+++ prog1_mandelbrot_threads/plot.py
@@ -1,16 +1,17 @@
+import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-file_path = 'results.csv'
-data = pd.read_csv(file_path)
+for file_path in ['results_view1.csv', 'results_view2.csv']:
+    data = pd.read_csv(file_path)
 
-data.columns = ['threads', 'time']
+    data.columns = ['threads', 'time']
 
-plt.figure(figsize=(10, 6))
-plt.plot(data['threads'], data['time'], marker='o', linestyle='-', label='Execution Time')
-plt.title('Execution Time vs Number of Threads')
-plt.xlabel('Number of Threads')
-plt.ylabel('Execution Time (ms)')
-plt.legend()
-plt.savefig('prog1_mandelbrot_threads.png')
-plt.close()
+    plt.figure(figsize=(10, 6))
+    plt.plot(data['threads'], data['time'], marker='o', linestyle='-', label='Execution Time')
+    plt.title('Execution Time vs Number of Threads')
+    plt.xlabel('Number of Threads')
+    plt.ylabel('Execution Time (ms)')
+    plt.legend()
+    plt.savefig(os.path.splitext(file_path)[0] + '.png')
+    plt.close()
